f4f2909605947e8ee457789494435e430388fb12
skhalozulu <214529134@stu.ukzn.ac.za>
2016-03-28 06:02:02 +0200

    dad class

---
+++ ConsoleApplication4/Header.h
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+#ifndef	HEADER_H
+#define HEADER_H
+class Dad
+{
+public:
+	Dad();
+	void show();
+	void showAgain();
+};
+#endif
---
+++ ConsoleApplication4/Source.cpp
@@ -0,0 +1,15 @@
+#include "Header.h"
+Dad::Dad()
+{
+
+}
+
+void Dad::show()
+{
+	cout << "Ngingu Ndabezitha " << endl;
+
+}
+void Dad::showAgain()
+{
+	cout << "Usthuli sika Ndaba" << endl;
+}
